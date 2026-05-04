c89d439fc931f58823c022913e6d87c90d559101
Yi Xuan Tan <yixuan.tan@lionsbot.com>
2026-05-04 09:51:34 +0800

    feat: speed up dataset loading with custom dataset class

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -29,6 +29,56 @@ class PreprocessedDataset(Dataset, Generic[TOutput]):
     def __len__(self):
         return len(self.dataset)
 
+class SkipEpisodesLeRobotDataset(Dataset):
+    """
+    Wrapper for `LeRobotDataset` that skips dirty episodes listed in the `meta/removed_episodes.json` file.
+    """
+    REMOVED_EPISODES_PATH = "meta/removed_episodes.json"
+    REMOVED_EPISODES_KEY = "dirty_episodes"
+
+    def __init__(self, *args, **kwargs):
+        self.lerobot_ds = LeRobotDataset(*args, **kwargs)
+        
+        rm_ep_path = pathlib.Path(self.lerobot_ds.root / self.REMOVED_EPISODES_PATH)
+        if rm_ep_path.exists():
+            with open(self.lerobot_ds.root / self.REMOVED_EPISODES_PATH) as f:
+                self.skip_episodes = json.load(f)[self.REMOVED_EPISODES_KEY]
+            self.skip_ranges = [
+                range(row["dataset_from_index"], row["dataset_to_index"])
+                for row in self.lerobot_ds.meta.episodes if row["episode_index"] in self.skip_episodes
+            ]
+            # obtain ranges to keep
+            self.keep_old_ranges = [
+                range(0, self.skip_ranges[0].start),
+                *[
+                    range(prev_range.stop, curr_range.start)
+                    for prev_range, curr_range in zip(self.skip_ranges[:-1], self.skip_ranges[1:])
+                    if prev_range.stop < curr_range.start
+                ],
+                range(self.skip_ranges[-1].stop, len(self.lerobot_ds)),
+            ]
+        else:
+            self.skip_episodes = []
+            self.skip_ranges = []
+            self.keep_old_ranges = [range(0, len(self.lerobot_ds))]
+        # map new indices to old indices
+        self.new_ranges_to_old_offsets = {}
+        new_range_start = 0
+        for old_range in self.keep_old_ranges:
+            new_range = range(new_range_start, new_range_start + len(old_range))
+            self.new_ranges_to_old_offsets[new_range] = old_range.start - new_range_start
+            new_range_start += len(old_range)
+
+    def __len__(self) -> int:
+        return list(self.new_ranges_to_old_offsets)[-1].stop
+
+    def __getitem__(self, idx: int) -> dict:
+        # find the range that contains the new index
+        for new_range, old_offset in self.new_ranges_to_old_offsets.items():
+            if idx in new_range:
+                return self.lerobot_ds[idx + old_offset]
+        raise IndexError(f"Index {idx} out of range")
+
 @dataclass
 @lerobot.processor.ProcessorStepRegistry.register("resample_action_processor")
 class ResampleActionProcessorStep(lerobot.processor.ProcessorStep):
@@ -234,15 +284,20 @@ def load_dataset(
     with tqdm(task_roots, desc=f"Loading ds — {root}") as tqdm_task_roots:
         for task_root in tqdm_task_roots:
             tqdm_task_roots.set_description(f"Loading ds — {root}/{task_root.relative_to(root)}")
-            lerobot_ds = load_lerobot_dataset_skip_dirty_episodes(
+            ds = SkipEpisodesLeRobotDataset(
                 str(task_root.relative_to(root)),
                 root=task_root,
                 delta_timestamps=delta_timestamps,
             )
+            # lerobot_ds = load_lerobot_dataset_skip_dirty_episodes(
+            #     str(task_root.relative_to(root)),
+            #     root=task_root,
+            #     delta_timestamps=delta_timestamps,
+            # )
             task_config = load_task_config(task_root)
             subset_inst_transform = functools.partial(
                 instance_transform,
-                meta=lerobot_ds.meta,
+                meta=ds.lerobot_ds.meta,
                 task_config=task_config,
             )
             preprocessor = PolicyProcessorPipeline(
@@ -250,7 +305,7 @@ def load_dataset(
                 to_transition=lambda b, inst_transform=subset_inst_transform: 
                     lerobot.processor.converters.batch_to_transition(inst_transform(b))
             )
-            preprocessed_subsets.append(PreprocessedDataset(lerobot_ds, preprocessor))
+            preprocessed_subsets.append(PreprocessedDataset(ds, preprocessor))
         tqdm_task_roots.set_description(f"Done loading ds — {root}")
 
     return ConcatDataset(preprocessed_subsets)
